48b370a777380758a35f198cf4d73e320ea156a3
DOMinator.jsx <parthbrajput30@gmail.com>
2026-01-12 07:59:14 +0530

    Fixed the npm amd pm2 not found issue in the VM.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='w-full h-screen flex items-center justify-center text-xl '>Hey my name is Parth and I am full stack web developer and I have the package of 1 CR.</div>
+    <div className='w-full h-screen flex items-center justify-center text-xl '>Hey my name is Parth and I am full stack web developer and I have the package of 1 CR. </div>
   )
 }
 
